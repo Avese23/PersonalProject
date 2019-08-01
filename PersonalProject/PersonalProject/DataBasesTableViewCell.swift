88528bcfe69fa1aa3ff6164cc24c495736a4e6be
Apple <apple@Apples-MacBook-Pro-107.local>
2019-08-01 09:56:36 -0400

    GivingUpOnMyComputerRIP

---
+++ PersonalProject/PersonalProject/DataBasesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DataBasesTableViewCell.swift
+//  PersonalProject
+//
+//  Created by Apple on 8/1/19.
+//  Copyright © 2019 AveryGraceEx. All rights reserved.
+//
+
+import UIKit
+
+class DataBasesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PersonalProject/PersonalProject/NameDatabaseViewController.swift
@@ -0,0 +1,36 @@
+//
+//  NameDatabaseViewController.swift
+//  PersonalProject
+//
+//  Created by Apple on 8/1/19.
+//  Copyright © 2019 AveryGraceEx. All rights reserved.
+//
+
+import UIKit
+
+class NameDatabaseViewController: UIViewController {
+
+  
+    @IBAction func NameThouDataBase(_ sender: UITextField) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        @IBAction func SaveDataBase(_ sender: UIButton) {
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+    
+ 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
